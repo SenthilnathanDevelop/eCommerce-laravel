9f64c57cfafffda0d65ef409ce41f90fb25ce283
senthil00793 <senthil@digitfellas.com>
2022-01-21 10:19:17 +0530

    id

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -16,7 +16,7 @@ class UserController extends Controller
             return "Username or Password is not matched";
         } else {
             $req->session()->put('user',$user);
-            return redirect('/');
+            return redirect('/product');
         }
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -20,4 +20,5 @@ Route::get('/login', function () {
 });
 
 Route::post("/login",[UserController::class,'login']);
-Route::get('/',[ProductController::class,'index']);
+Route::get('/product',[ProductController::class,'index']);
+Route::get('/detail/{id}',[ProductController::class,'detail']);
