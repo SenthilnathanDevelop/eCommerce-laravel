5140f46b4c206488b1b9b608fc5e422cf3bddfcc
senthil00793 <senthil@digitfellas.com>
2022-01-21 11:50:39 +0530

    add to cart

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Product;
+use App\Models\Cart;
+use Session;
 
 class ProductController extends Controller
 {
@@ -15,6 +17,25 @@ class ProductController extends Controller
     }
     function detail($id)
     {
-        return Product::find($id);
+        $data = Product::find($id);
+        return view('detail',['product' => $data]);
+    }
+    function addToCart(Request $request)
+    {
+      if($request->session()->has('user'))
+      {
+        $cart = new Cart;
+        $cart->user_id = $request->session()->get('user')['id'];
+        $cart->product_id = $request->product_id;
+        $cart->save();
+        return redirect("/product");
+      } else {
+        return redirect("/login");
+      }
+    }
+    function cartItem()
+    {
+      $user_id = Session::get('user')['id'];
+      return Cart::where('user_id',$user_id)->count();
     }
 }
--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -40,5 +40,8 @@
   .trending-wrapper {
     margin: 30px;
   }
+  .detail-img {
+    height: 200px;
+  }
 </style>
 </html>
